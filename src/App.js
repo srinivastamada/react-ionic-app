1a64045fc8b985277dcc33335e4f94e0d1b7186f
Srinivas Tamada <srinivas@9lessons.info>
2019-03-07 10:51:30 -0500

    Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import '@ionic/core/css/core.css';
 import '@ionic/core/css/ionic.bundle.css';
-import { IonApp, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent } from '@ionic/react';
+import {
+  IonApp,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent,
+  IonFooter,
+  IonHeader,
+  IonToolbar,
+  IonBackButton,
+  IonButtons,
+  IonTitle
+} from '@ionic/react';
 import React, { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
       <IonApp>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonBackButton goBack={() => {}} />
+            </IonButtons>
+            <IonTitle>My Navigation Bar</IonTitle>
+          </IonToolbar>
+
+          <IonToolbar>
+            <IonTitle>Subheader</IonTitle>
+          </IonToolbar>
+        </IonHeader>
         <IonContent>
           <IonCard>
             <IonCardHeader>
@@ -15,6 +40,11 @@ class App extends Component {
             </IonCardHeader>
           </IonCard>
         </IonContent>
+        <IonFooter>
+      <IonToolbar>
+        <IonTitle>Footer</IonTitle>
+      </IonToolbar>
+    </IonFooter>
       </IonApp>
     );
   }
